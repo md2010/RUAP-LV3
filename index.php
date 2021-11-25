f07c846df3d86ef8c6c46b3c04e5515acd710f65
md2010 <76563833+md2010@users.noreply.github.com>
2021-11-25 13:25:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -75,12 +75,12 @@ if (mysqli_connect_errno())
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$sql = 'CREATE Database db IF NOT EXISTS db';
+/*$sql = 'CREATE Database db IF NOT EXISTS db';
  if ($conn->query($sql) === TRUE) {
  echo "Database created successfully";
  } else {
  echo "Error creating database: " . $conn->error;
- }
+ }*/
  mysqli_select_db($conn,$db);
  /*$sql_create = "CREATE TABLE registration_tbl(id INT NOT NULL
  AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(30), email
