9146f9f2bc9f07d9c2c0f5e36a7cb2f1eeaaa9ad
md2010 <76563833+md2010@users.noreply.github.com>
2021-11-25 13:15:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -75,6 +75,23 @@ if (mysqli_connect_errno())
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
+$sql = 'CREATE Database ruapdb IF NOT EXISTS ruapdb';
+ if ($conn->query($sql) === TRUE) {
+ echo "Database created successfully";
+ } else {
+ echo "Error creating database: " . $conn->error;
+ }
+ mysqli_select_db($conn,$db);
+ $sql_create = "CREATE TABLE registration_tbl(id INT NOT NULL
+ AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(30), email
+ VARCHAR(30), date DATE);";
+ if ($conn->query($sql_create) === TRUE) {
+ echo "Table registration_tbl created successfully";
+ } else {
+ echo "Error creating table: " . $conn->error;
+ }
+ $conn->close();
+?>
 // Insert registration info
 if (!empty($_POST))
 {
